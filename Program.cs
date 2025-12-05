0fa3b5c7e566db2b023681ff0061f948aa5c393a
Gungyphant <gungyphant@gmail.com>
2025-12-05 07:05:30 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿namespace AOC2025Day5
+{
+    public class Program
+    {
+        public static string PartOne(string data)
+        {
+            return "";
+        }
+        public static string PartTwo(string data)
+        {
+            return "";
+        }
+        static void Main()
+        {
+            string file = File.ReadAllText(@"../../../input.txt");
+            Console.WriteLine(PartOne(file));
+            Console.WriteLine(PartTwo(file));
+        }
+    }
+}
