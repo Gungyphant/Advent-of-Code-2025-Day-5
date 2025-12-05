61226b55dff70632e52a911dc691bf3fdb2f003d
Gungyphant <gungyphant@gmail.com>
2025-12-05 08:13:20 +0000

    Created working solution for Part One and too-inefficient solution for Part Two

--- Program.cs
+++ Program.cs
@@ -4,11 +4,69 @@
     {
         public static string PartOne(string data)
         {
-            return "";
+            bool areAvaliableLines = false;
+            List<Tuple<long, long>> ranges = new List<Tuple<long, long>>();
+            List<long> availableIDs = new List<long>();
+            foreach (string line in data.Split(Environment.NewLine))
+            {
+                if (line == "")
+                {
+                    areAvaliableLines = true;
+                }
+                else if (areAvaliableLines)
+                {
+                    availableIDs.Add(Convert.ToInt64(line));
+                }
+                else
+                {
+                    long startID = Convert.ToInt64(line.Split("-")[0]);
+                    long endID = Convert.ToInt64(line.Split("-")[1]);
+                    ranges.Add(new Tuple<long, long>(startID, endID));
+                }
+            }
+            int freshIDs = 0;
+            foreach (long id in availableIDs)
+            {
+                foreach (Tuple<long, long> range in ranges)
+                {
+                    long startID = range.Item1;
+                    long endID = range.Item2;
+                    if (id >= startID && id <= endID)
+                    {
+                        freshIDs++;
+                        break;
+                    }
+                }
+            }
+            return Convert.ToString(freshIDs);
         }
         public static string PartTwo(string data)
         {
-            return "";
+            List<long> foundFresh = new List<long>();
+            int n = 0;
+            int length = data.Split(Environment.NewLine).Length;
+            foreach (string line in data.Split(Environment.NewLine))
+            {
+                Console.WriteLine(n/length);
+                if (line == "")
+                {
+                    break;
+                }
+                else
+                {
+                    long startID = Convert.ToInt64(line.Split("-")[0]);
+                    long endID = Convert.ToInt64(line.Split("-")[1]);
+                    for (long freshID = startID; freshID <= endID; freshID++)
+                    {
+                        if (!foundFresh.Contains(freshID))
+                        {
+                            foundFresh.Add(freshID);
+                        }
+                    }
+                }
+                n++;
+            }
+            return Convert.ToString(foundFresh.Count);
         }
         static void Main()
         {
